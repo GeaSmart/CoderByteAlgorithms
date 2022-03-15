aff6db3ddeb90bbe4c712a927e34709cba007270
Gerson Azabache Martinez <gerson_dunk@hotmail.com>
2022-03-15 17:49:43 -0500

    longest word algorithm added

---
+++ CoderByte/Algorithms/LongestWord.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CoderByte.Algorithms
+{
+    public class LongestWord
+    {
+        public static string Run(string texto)
+        {
+            texto = new string((from l in texto where char.IsWhiteSpace(l) || char.IsLetterOrDigit(l) select l).ToArray());
+
+            int maxCount = 0;            
+            string maxPalabra = "";
+            string[] palabras = texto.Split(" ");
+
+            foreach(var palabra in palabras)
+            {
+                int contador = 0;
+
+                foreach(char letra in palabra)
+                {
+                    contador++;
+                }
+
+                if(contador > maxCount)
+                {
+                    maxCount = contador;
+                    maxPalabra = palabra;
+                }
+
+            }
+
+            return maxPalabra;
+        }
+    }
+}
---
+++ CoderByte/Program.cs
@@ -0,0 +1,13 @@
+﻿using CoderByte.Algorithms;
+using System;
+
+namespace CoderByte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LongestWord.Run("heyy hola2! estoe"));
+        }
+    }
+}
