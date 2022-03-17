45cad5bf7ff5f0f6e8be5fe2f36e75631c1e08d1
Gerson Azabache Martinez <gerson_dunk@hotmail.com>
2022-03-16 19:35:13 -0500

    codelandusernamevalidation alg

---
+++ CoderByte/Algorithms/CodelandUsernameValidation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CoderByte.Algorithms
+{
+    public class CodelandUsernameValidation
+    {
+        public static string Run(string str)
+        {
+            if (str.Length < 4 || str.Length > 25)
+                return "false";
+
+            if (!char.IsLetter(str[0]))
+                return "false";
+
+            if (str[str.Length - 1] == '_')
+                return "false";
+
+            var x = new string((from l in str where !char.IsLetterOrDigit(l) && l != '_' select l).ToArray());
+
+            if (x.Length > 0)
+                return "false";
+
+            return "true";
+        }
+    }
+}
